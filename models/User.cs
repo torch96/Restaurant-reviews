b11359481804b7268d9ae66ba48be5f49b98954a
Jospeh <47431708+torch96@users.noreply.github.com>
2022-08-12 23:18:07 -0400

    first commit

---
+++ models/User.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace Restaurant.Models
+{
+    public class User{
+        [BsonElement("_id")]
+        [JsonProperty("_id")]
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [JsonProperty("auth_token")]
+        [BsonIgnore]
+        public string AuthToken { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+        [JsonIgnore]
+        public string HashedPassword { get; set; }
+
+    }
+
+    public class UserLogin{
+        [Required]
+        [EmailAddress]
+        private string Email { get; set; }
+        [Required]
+        private string Password { get; set; }
+    }
+}
